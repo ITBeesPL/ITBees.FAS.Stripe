26c89778e3491ddfb39b2a100e91390370ccd89d
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-07-05 18:00:05 +0200

    Add project files.

---
+++ ITBees.FAS.Stripe/FasStripePaymentProcessor.cs
@@ -0,0 +1,56 @@
+﻿using ITBees.FAS.Payments;
+using ITBees.FAS.Payments.Interfaces;
+using ITBees.Interfaces.Platforms;
+using Stripe;
+using Stripe.Checkout;
+
+namespace ITBees.FAS.Stripe
+{
+    public class FasStripePaymentProcessor : IFasPaymentProcessor
+    {
+        private readonly IPlatformSettingsService _platformSettingsService;
+
+        public FasStripePaymentProcessor(IPlatformSettingsService platformSettingsService)
+        {
+            _platformSettingsService = platformSettingsService;
+            StripeSettings = new StripeSettings(platformSettingsService);
+            StripeConfiguration.ApiKey = StripeSettings.SecretKey;
+        }
+
+        public FasActivePaymentSession CreatePaymentSession(FasPayment fasPayment)
+        {
+            var options = new SessionCreateOptions()
+            {
+                SuccessUrl = _platformSettingsService.GetSetting("PaymentSuccessUrl"),
+                CancelUrl = _platformSettingsService.GetSetting("PaymentCancelUrl"),
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode = fasPayment.Mode.ToString().ToLower(),
+            };
+
+            foreach (var product in fasPayment.Products)
+            {
+                options.LineItems.Add(new SessionLineItemOptions()
+                {
+                    PriceData = new SessionLineItemPriceDataOptions()
+                    {
+                        UnitAmount = product.Quantity,
+                        Currency = product.Currency,
+                        ProductData = new SessionLineItemPriceDataProductDataOptions()
+                        {
+                            Name = product.PaymentTitleOrProductName,
+                        }
+                    },
+                    Quantity = product.Quantity
+
+                });
+            }
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            return new FasActivePaymentSession(session.Url);
+        }
+
+        public StripeSettings StripeSettings { get; }
+    }
+}
---
+++ ITBees.FAS.Stripe/StripeSettings.cs
@@ -0,0 +1,15 @@
+﻿using ITBees.Interfaces.Platforms;
+
+namespace ITBees.FAS.Stripe;
+
+public class StripeSettings
+{
+    public StripeSettings(IPlatformSettingsService platformSettingsService)
+    {
+        SecretKey = platformSettingsService.GetSetting("StripeSecretKey");
+        PublishableKey = platformSettingsService.GetSetting("StripePublishableKey");
+    }
+
+    public string SecretKey { get; set; }
+    public string PublishableKey { get; set; }
+}
