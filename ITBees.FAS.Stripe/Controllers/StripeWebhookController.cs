1eae20064d2a182de6c74928d921e22af7151736
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-08-03 09:30:38 +0200

    Webhook with logging

--- ITBees.FAS.Stripe/Controllers/StripeWebhookController.cs
+++ ITBees.FAS.Stripe/Controllers/StripeWebhookController.cs
@@ -1,4 +1,5 @@
 ﻿using ITBees.FAS.Payments.Interfaces;
+using ITBees.FAS.Payments.Interfaces.Models;
 using ITBees.Interfaces.Platforms;
 using ITBees.RestfulApiControllers;
 using Microsoft.AspNetCore.Mvc;
@@ -10,16 +11,21 @@ namespace ITBees.FAS.Stripe.Controllers;
 
 public class StripeWebhookController : RestfulControllerBase<StripeWebhookController>
 {
+    private readonly ILogger<StripeWebhookController> _logger;
     private readonly IPaymentSessionCreator _paymentSessionCreator;
     private readonly IPlatformSettingsService _platformSettingsService;
+    private readonly IPaymentDbLoggerService _paymentDbLoggerService;
     private readonly string _webhookSecret;
 
-    public StripeWebhookController(ILogger<StripeWebhookController> logger, 
+    public StripeWebhookController(ILogger<StripeWebhookController> logger,
         IPaymentSessionCreator paymentSessionCreator,
-        IPlatformSettingsService platformSettingsService) : base(logger)
+        IPlatformSettingsService platformSettingsService,
+        IPaymentDbLoggerService paymentDbLoggerService) : base(logger)
     {
+        _logger = logger;
         _paymentSessionCreator = paymentSessionCreator;
         _platformSettingsService = platformSettingsService;
+        _paymentDbLoggerService = paymentDbLoggerService;
         _webhookSecret = platformSettingsService.GetSetting("StripeWebhookKey");
     }
 
@@ -28,7 +34,7 @@ public class StripeWebhookController : RestfulControllerBase<StripeWebhookContro
     {
         var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
         var stripeEvent = ParseEvent(json, Request.Headers["Stripe-Signature"]);
-
+        _paymentDbLoggerService.Log(new PaymentOperatorLog() { Event = stripeEvent.Type, Received = DateTime.Now, Operator = "Stripe webhook", JsonEvent = json });
         if (stripeEvent.Type == Events.CheckoutSessionCompleted)
         {
             var session = stripeEvent.Data.Object as Session;
@@ -46,7 +52,15 @@ public class StripeWebhookController : RestfulControllerBase<StripeWebhookContro
         }
         catch (StripeException e)
         {
+            _paymentDbLoggerService.Log(new PaymentOperatorLog()
+            {
+                JsonEvent = json,
+                Operator = "Stripe webhook",
+                Received = DateTime.Now,
+                Event = $"Webhook error ! {e.Message}"
+            });
             Response.StatusCode = 400;
+            _logger.LogError(e, json);
             throw new Exception("Webhook verification failed", e);
         }
     }
